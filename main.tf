b4f129b1b1dede68177c2097d26191c83daea7d2
abhishekk2407 <abhishekkumarsingh2407@gmail.com>
2022-11-27 13:35:24 +0000

    updating terraform config

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.44.1"
     }
   }
   backend "gcs" {
-    bucket      = "lz_1527902_tfstate_files"
-    prefix = "=backend/windows-jenkins/tfstate/"
+    bucket = "lz_1527902_tfstate_files"
+    prefix = "backend/windows-jenkins/tfstate/"
   }
 }
 
 provider "google" {
-    credentials="key.json"
+  credentials = "key.json"
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "project_id" {
-  type=string
-    default = "kf-org-auto-billing"
+  type    = string
+  default = "kf-org-auto-billing"
 }
 
-variable "bucket_name"{
-type = string
-default = "lz-1527902-jenkins-pipeline-bucket-default-name"
+variable "bucket_name" {
+  type    = string
+  default = "lz-1527902-jenkins-pipeline-bucket-default-name"
 }
 
-variable "bucket_location"{
-type = string
-default = "europe-west4"
+variable "bucket_location" {
+  type    = string
+  default = "europe-west4"
 }
