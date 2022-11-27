686394d03c171c5329e55e65b4d53e66d269694a
abhishekk2407 <abhishekkumarsingh2407@gmail.com>
2022-11-27 07:30:27 +0000

    First Commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.44.1"
+    }
+  }
+ 
+}
+
+provider "google" {
+    credentials="key.json"
+}
