1ae864002066cf94c8f412fa4c3c6f6682f8cd2c
abhishekk2407 <abhishekkumarsingh2407@gmail.com>
2022-11-28 07:02:13 +0000

    updating main.tf

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ terraform {
   backend "gcs" {
     bucket = "lz_1527902_jenkins_pipeline_tfstate"
     prefix = "terraform/tfstate"
-    credentials = "key.json"
+    /* credentials = "key.json" */
   }
 }
 
 provider "google" {
-  credentials = "key.json"
+  /* credentials = "key.json" */
   project = var.project_id
 }
