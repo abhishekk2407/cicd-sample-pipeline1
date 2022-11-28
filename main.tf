a87947b6d5cef9039bad98ffc88d4b7ab0529ba1
abhishekk2407 <abhishekkumarsingh2407@gmail.com>
2022-11-28 06:51:15 +0000

    Updating terraform config

--- main.tf
+++ main.tf
@@ -2,15 +2,16 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.44.1"
     }
   }
   backend "gcs" {
-    bucket = "lz_1527902_tfstate_files"
-    prefix = "backend/windows-jenkins/tfstate/"
+    bucket = "lz_1527902_jenkins_pipeline_tfstate"
+    prefix = "terraform/tfstate"
+    credentials = "key.json"
   }
 }
 
 provider "google" {
   credentials = "key.json"
+  project = var.project_id
 }
